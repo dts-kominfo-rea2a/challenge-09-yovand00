169e575f7c474e316e0f56af0112c026a47c9340
chicken on fire <yesaya.yova@gmail.com>
2022-06-26 14:02:17 +0700

    initial commit by Yesaya Yova

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,18 @@
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
 
-const Contact = () => {
+const Contact = (props) => {
     return (
-        <></>
+        <div style={{border: '1px solid green', width: '300px', margin:'10px auto', borderRadius: '15px', fontFamily: 'verdana', backgroundColor: '#72aaed'}}>
+            <div className="contact" style={{padding: '10px', textAlign: 'center',  display:'inline'}}>
+                <div className="name">{props.name}</div>
+                <div className="telp">{props.telp}</div>
+                <div className='email'>{props.email}</div>
+            </div>
+            <div style={{display:'inline'}}>
+                <a href={props.photo}></a>
+            </div>
+        </div>
     )
 }
 
